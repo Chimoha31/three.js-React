32e013ef8a83af75e23663ead1c182dde4909af7
Chimoha31 <mokochii1108@gmail.com>
2022-04-27 18:35:35 -0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import { Image } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Image, Scroll, ScrollControls, useScroll } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useRef } from "react";
 import "./App.css";
 
 function Images() {
+  const { width, height } = useThree((state) => state.viewport);
+  const group = useRef();
+  const data = useScroll();
+
+  useFrame(() => {
+    for (let i = 0; i < 3; i++) {
+      group.current.children[i].material.zoom = 1 + data.range(0, 1 / 3) / 3;
+    }
+  });
+
   return (
-    <group>
-      <Image url="./img1.jpg" scale={[4, 1, 1]} />
+    <group ref={group}>
+      <Image url="./img1.jpg" scale={[4, height, 1]} position={[-1, 0, 1]} />
+      <Image url="./img2.jpg" scale={3} position={[2, 0, 1]} />
+      <Image
+        url="./img3.jpg"
+        scale={[1, 3.5, 1]}
+        position={[-2.3, -height, 2]}
+      />
+      <Image
+        url="./img4.jpg"
+        scale={[1.4, 2, 1]}
+        position={[1.3, -height, -0.3, 3.2]}
+      />
     </group>
   );
 }
@@ -13,7 +35,19 @@ function Images() {
 function App() {
   return (
     <Canvas>
-      <Images />
+      <ScrollControls pages={2} damping={3}>
+        <Scroll>
+          <Images />
+        </Scroll>
+        <Scroll html>
+          <h1 style={{ position: "absolute", top: "60vh", left: "1.5em" }}>
+            Be
+          </h1>
+          <h1 style={{ position: "absolute", top: "140vh", left: "40vw" }}>
+            Creative
+          </h1>
+        </Scroll>
+      </ScrollControls>
     </Canvas>
   );
 }
